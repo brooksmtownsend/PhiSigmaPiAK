2da2fe0c560d4cebc966e1c1f781987db85df893
Brooks Townsend <brooksmtownsend@gmail.com>
2018-01-27 21:38:09 -0500

    added firebase support for android

--- android/app/src/main/java/com/phisigmapialphakappachapter/MainApplication.java
+++ android/app/src/main/java/com/phisigmapialphakappachapter/MainApplication.java
@@ -1,10 +1,9 @@
-package com.firstlookreactnavigation;
+package com.phisigmapialphakappachapter;
 
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import io.invertase.firebase.RNFirebasePackage;
-import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -27,7 +26,6 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new RNFirebasePackage(),
-            new RCTCameraPackage(),
             new VectorIconsPackage()
       );
     }
