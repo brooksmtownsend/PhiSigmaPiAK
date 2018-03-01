755576cc83754ddf0a8761bccdf6376510a3b011
Brooks Townsend <brooksmtownsend@gmail.com>
2018-03-01 18:05:59 -0500

    Added dependencies for Enzyme, working shallow rendering

--- __tests__/YellowPages.test.js
+++ __tests__/YellowPages.test.js
@@ -2,21 +2,22 @@ var React = require('react');
 var ReactNative = require('react-native');
 var renderer = require('react-test-renderer');
 var jest = require('jest');
+import { shallow, configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
 
-import { Root } from '../app/config/router'
-import App from '../app/index';
-import Attendance from '../app/screens/Attendance'
-import UsefulLinks from '../app/screens/UsefulLinks'
 import YellowPages from '../app/screens/YellowPages'
 import UserDetail from '../app/screens/UserDetail'
+
+import { ScrollView } from 'react-native'
 
 describe("YellowPages Functionality", () => {
   it("renders correctly", () => {
     const yellowPages = renderer.create(<YellowPages />).toJSON()
     expect(yellowPages).toMatchSnapshot()
   })
-  it("maps directory items to tiles", () => {
-    const yellowPages = renderer.create(<YellowPages />)
-    expect(yellowPages).not.toBeNull()
+  it("renders a ScrollView", () => {
+    const yellowPages = shallow(<YellowPages />)
+    expect(yellowPages.find(ScrollView)).toHaveLength(1)
   })
 })
