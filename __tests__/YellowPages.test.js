69cbdc74d47cc14465298c5e56a87978f5aa80f5
Brooks Townsend <brooksmtownsend@gmail.com>
2018-03-01 18:46:50 -0500

    More robust testing for YellowPages

--- __tests__/YellowPages.test.js
+++ __tests__/YellowPages.test.js
@@ -8,8 +8,10 @@ configure({ adapter: new Adapter() });
 
 import YellowPages from '../app/screens/YellowPages'
 import UserDetail from '../app/screens/UserDetail'
+import { directory } from '../app/data/directory'
 
 import { ScrollView } from 'react-native'
+import { List, ListItem } from 'react-native-elements'
 
 describe("YellowPages Functionality", () => {
   it("renders correctly", () => {
@@ -20,4 +22,38 @@ describe("YellowPages Functionality", () => {
     const yellowPages = shallow(<YellowPages />)
     expect(yellowPages.find(ScrollView)).toHaveLength(1)
   })
+  it("renders a List", () => {
+    const yellowPages = shallow(<YellowPages />)
+    expect(yellowPages.find(List)).toHaveLength(1)
+  })
+  it("renders a ListItem for each person in directory", () => {
+    const yellowPages = shallow(<YellowPages />)
+    expect(yellowPages.find(ListItem)).toHaveLength(directory.length)
+  })
+  it("renders a ListItem for Abby with correct properties", () => {
+    const yellowPages = shallow(<YellowPages />)
+    const abby = directory[0]
+    expect(yellowPages.contains(
+      <ListItem 
+        key={abby.Email}
+        title={abby.FirstName + " " + abby.LastName}
+        subtitle={abby.Email}
+        onPress={() => this.onLearnMore(person)}
+      />
+    ))
+  })
+  it("renders a ListItem for a random person with correct properties", () => {
+    const yellowPages = shallow(<YellowPages />)
+    const randomIndex = Math.floor(Math.random() * directory.length)
+    const person = directory[randomIndex]
+    expect(yellowPages.contains(
+      <ListItem 
+        key={person.Email}
+        title={person.FirstName + " " + person.LastName}
+        subtitle={person.Email}
+        onPress={() => this.onLearnMore(person)}
+      />
+    ))
+  })
+
 })
